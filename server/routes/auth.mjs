706e4608bae07a5740a8b20cf6d7e658682d49aa
bobbyshnoll <charles-o.tr@hotmail.com>
2023-05-18 08:30:41 -0400

    Auth backend

---
+++ server/routes/auth.mjs
@@ -0,0 +1,108 @@
+import express from 'express';
+import db from '../db/conn.mjs';
+import { ObjectId } from 'mongodb';
+import jsonwebtoken from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const HASH_ITERATIONS = 10;
+
+const router = express.Router();
+
+//FIXME ROUTE TRÈS TEMPORAIRE POUR AVOIR LA LISTE DES USERS
+//TODO ENLEVER OU SÉCURISER
+router.get('/', async (req, res) => {
+    const collection = db.collection('users');
+    const results = await collection.find({}).toArray();
+    res.send(results).status(200);
+});
+
+// This route registers a new user with a username, email adress and hashed password
+router.post('/register', async (req, res) => {
+    const user = req.body;
+
+    const takenUsername = await db.collection('users').findOne({ username: user.username });
+    const takenEmail = await db.collection('users').findOne({ email: user.email });
+
+    if (takenUsername) {
+        res.json({ message: 'Username has already been taken' });
+    } else if (takenEmail) {
+        res.json({ message: 'Email has already been taken' });
+    } else {
+        user.password = await bcrypt.hash(req.body.password, HASH_ITERATIONS);
+
+        const newUser = {
+            username: user.username,
+            email: user.email,
+            password: user.password,
+        };
+        const collection = db.collection('users');
+        const result = await collection.insertOne(newUser);
+        res.send(result).status(204);
+    }
+});
+
+// This route logs in a user by generating a jwt
+router.post('/login', (req, res) => {
+    const userLoggingIn = req.body;
+
+    db.collection('users')
+        .findOne({ username: userLoggingIn.username })
+        .then((dbUser) => {
+            if (!dbUser) {
+                return res.json({ message: 'Invalid username or password' });
+            }
+            bcrypt.compare(userLoggingIn.password, dbUser.password).then((isCorrect) => {
+                if (isCorrect) {
+                    const payload = {
+                        id: dbUser._id,
+                        username: dbUser.username,
+                    };
+                    jsonwebtoken.sign(
+                        payload,
+                        process.env.JWT_SECRET,
+                        { expiresIn: process.env.JWT_EXPIRES_IN },
+                        (err, token) => {
+                            if (err) return res.json({ message: 'err' });
+                            return res.json({
+                                message: 'Sucess',
+                                token: 'Bearer ' + token,
+                            });
+                        },
+                    );
+                } else {
+                    return res.json({
+                        message: 'Invalid username or password',
+                    });
+                }
+            });
+        });
+});
+
+// This function is called by the /getUsername route and validates the status of the JWT
+function verifyJWT(req, res, callback) {
+    const token = re.headers['x-access-token']?.split(' ')[1];
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET),
+            (err, decoded) => {
+                if (err)
+                    return res.json({
+                        isLoggedIn: false,
+                        message: 'Failed to authenticate',
+                    });
+                req.user = {};
+                req.user.id = decoded.id;
+                req.user.username = decoded.username;
+                callback();
+            };
+    } else {
+        res.json({ message: 'Incorrect token', isLoggedIn: false });
+    }
+}
+
+// This route validates the login status and returns user info
+router.get('/getUser', verifyJWT, (req, res) => {
+    res.json({ isLoggedIn: true, username: req.user.username });
+});
+
+export default router;
--- server/server.mjs
+++ server/server.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import './loadEnvironment.mjs';
 import records from './routes/record.mjs';
+import auth from './routes/auth.mjs';
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/record', records);
+app.use('/auth', auth);
 
 // start the Express server
 app.listen(PORT, () => {
